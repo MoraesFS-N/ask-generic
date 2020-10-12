f676d285c17bda5568d3b2aa7bb24a67da7ca487
Moraes Felicio da Silva Neto <moraes.neto@mercovia-sa.com>
2020-10-12 12:23:30 -0300

    utilizando else no EJS

--- index.js
+++ index.js
@@ -4,14 +4,25 @@ const app = express();
 app.set('view engine', 'ejs');
 
 
-app.get("/", (req, res) => {
-    res.render('index');
+app.get("/:nome/:idade", (req, res) => {
+
+    var nome = req.params.nome;
+    var idade = req.params.idade;
+    var exibeMsg = false;
+
+    res.render('index', {
+        nome: nome,
+        idade: idade,
+        empresa: "DB - System",
+        faculdade: "Sistemas de informção iffar",
+        msg: exibeMsg
+    });
 })
 
 app.listen(8080, (err) => {
     if (err) {
         console.log(err);
     } else {
-        console.log("App rodando");
+        console.log("App runnig at port 8080");
     }
 })
