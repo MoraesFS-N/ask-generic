57a32e81d713b3cabc6a259fe00a50c868608f26
Moraes Felicio da Silva Neto <moraes.neto@mercovia-sa.com>
2020-10-13 08:52:10 -0300

    implementando bootstrap no projeto

--- index.js
+++ index.js
@@ -5,42 +5,8 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 
-app.get("/:nome/:idade", (req, res) => {
-
-    var nome = req.params.nome;
-    var idade = req.params.idade;
-    var exibeMsg = false;
-
-    var produtos = [{
-            nome: "Pastel",
-            preco: 5.00
-        },
-        {
-            nome: "Enroladinho",
-            preco: 4.00
-        },
-        {
-            nome: "Bolo",
-            preco: 3.00
-        },
-        {
-            nome: "Refrigerante",
-            preco: 7.00
-        },
-        {
-            nome: "Salgadinho",
-            preco: 5.00
-        }
-    ]
-
-    res.render('index', {
-        nome: nome,
-        idade: idade,
-        empresa: "DB - System",
-        faculdade: "Sistemas de informção iffar",
-        msg: exibeMsg,
-        produtos: produtos
-    });
+app.get("/", (req, res) => {
+    res.render('index');
 })
 
 app.listen(8080, (err) => {
